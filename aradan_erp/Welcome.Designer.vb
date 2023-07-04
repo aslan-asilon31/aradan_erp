f6c3e21d463990ed9b3ac5ede85608cf56c82f82
aslan-asilon3 <sulaslansetiawan3@gmail.com>
2023-07-04 03:05:08 -0700

    first commit

---
+++ aradan_erp/Welcome.Designer.vb
@@ -0,0 +1,233 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Welcome
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.inventoryicon = New System.Windows.Forms.PictureBox()
+        Me.realestateicon = New System.Windows.Forms.PictureBox()
+        Me.payrollicon = New System.Windows.Forms.PictureBox()
+        Me.hricon = New System.Windows.Forms.PictureBox()
+        Me.fleeticon = New System.Windows.Forms.PictureBox()
+        Me.biicon = New System.Windows.Forms.PictureBox()
+        Me.ecommerceicon = New System.Windows.Forms.PictureBox()
+        Me.salesicon = New System.Windows.Forms.PictureBox()
+        Me.crmicon = New System.Windows.Forms.PictureBox()
+        Me.posicon = New System.Windows.Forms.PictureBox()
+        Me.productionicon = New System.Windows.Forms.PictureBox()
+        Me.purchaseicon = New System.Windows.Forms.PictureBox()
+        CType(Me.inventoryicon, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.realestateicon, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.payrollicon, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.hricon, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.fleeticon, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.biicon, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ecommerceicon, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.salesicon, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.crmicon, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.posicon, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.productionicon, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.purchaseicon, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'inventoryicon
+        '
+        Me.inventoryicon.BackgroundImage = Global.aradan_erp.My.Resources.Resources.inventory
+        Me.inventoryicon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.inventoryicon.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.inventoryicon.Location = New System.Drawing.Point(300, 76)
+        Me.inventoryicon.Name = "inventoryicon"
+        Me.inventoryicon.Size = New System.Drawing.Size(58, 50)
+        Me.inventoryicon.TabIndex = 0
+        Me.inventoryicon.TabStop = False
+        '
+        'realestateicon
+        '
+        Me.realestateicon.BackgroundImage = Global.aradan_erp.My.Resources.Resources.real_estate
+        Me.realestateicon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.realestateicon.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.realestateicon.Location = New System.Drawing.Point(429, 112)
+        Me.realestateicon.Name = "realestateicon"
+        Me.realestateicon.Size = New System.Drawing.Size(62, 50)
+        Me.realestateicon.TabIndex = 1
+        Me.realestateicon.TabStop = False
+        '
+        'payrollicon
+        '
+        Me.payrollicon.BackgroundImage = Global.aradan_erp.My.Resources.Resources.ac_and_payroll
+        Me.payrollicon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.payrollicon.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.payrollicon.Location = New System.Drawing.Point(537, 193)
+        Me.payrollicon.Name = "payrollicon"
+        Me.payrollicon.Size = New System.Drawing.Size(51, 50)
+        Me.payrollicon.TabIndex = 2
+        Me.payrollicon.TabStop = False
+        '
+        'hricon
+        '
+        Me.hricon.BackgroundImage = Global.aradan_erp.My.Resources.Resources.hr_and_admin
+        Me.hricon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.hricon.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.hricon.Location = New System.Drawing.Point(575, 308)
+        Me.hricon.Name = "hricon"
+        Me.hricon.Size = New System.Drawing.Size(51, 50)
+        Me.hricon.TabIndex = 3
+        Me.hricon.TabStop = False
+        '
+        'fleeticon
+        '
+        Me.fleeticon.BackgroundImage = Global.aradan_erp.My.Resources.Resources.fleet_management
+        Me.fleeticon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.fleeticon.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.fleeticon.Location = New System.Drawing.Point(510, 420)
+        Me.fleeticon.Name = "fleeticon"
+        Me.fleeticon.Size = New System.Drawing.Size(51, 50)
+        Me.fleeticon.TabIndex = 4
+        Me.fleeticon.TabStop = False
+        '
+        'biicon
+        '
+        Me.biicon.BackgroundImage = Global.aradan_erp.My.Resources.Resources.bi_and_analyze
+        Me.biicon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.biicon.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.biicon.Location = New System.Drawing.Point(415, 497)
+        Me.biicon.Name = "biicon"
+        Me.biicon.Size = New System.Drawing.Size(51, 50)
+        Me.biicon.TabIndex = 5
+        Me.biicon.TabStop = False
+        '
+        'ecommerceicon
+        '
+        Me.ecommerceicon.BackgroundImage = Global.aradan_erp.My.Resources.Resources.ecommerce
+        Me.ecommerceicon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ecommerceicon.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.ecommerceicon.Location = New System.Drawing.Point(288, 535)
+        Me.ecommerceicon.Name = "ecommerceicon"
+        Me.ecommerceicon.Size = New System.Drawing.Size(51, 50)
+        Me.ecommerceicon.TabIndex = 6
+        Me.ecommerceicon.TabStop = False
+        '
+        'salesicon
+        '
+        Me.salesicon.BackgroundImage = Global.aradan_erp.My.Resources.Resources.sales
+        Me.salesicon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.salesicon.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.salesicon.Location = New System.Drawing.Point(179, 497)
+        Me.salesicon.Name = "salesicon"
+        Me.salesicon.Size = New System.Drawing.Size(51, 50)
+        Me.salesicon.TabIndex = 7
+        Me.salesicon.TabStop = False
+        '
+        'crmicon
+        '
+        Me.crmicon.BackgroundImage = Global.aradan_erp.My.Resources.Resources.customer_relationship_management
+        Me.crmicon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.crmicon.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.crmicon.Location = New System.Drawing.Point(97, 420)
+        Me.crmicon.Name = "crmicon"
+        Me.crmicon.Size = New System.Drawing.Size(51, 50)
+        Me.crmicon.TabIndex = 8
+        Me.crmicon.TabStop = False
+        '
+        'posicon
+        '
+        Me.posicon.BackgroundImage = Global.aradan_erp.My.Resources.Resources.point_of_sale
+        Me.posicon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.posicon.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.posicon.Location = New System.Drawing.Point(67, 298)
+        Me.posicon.Name = "posicon"
+        Me.posicon.Size = New System.Drawing.Size(51, 50)
+        Me.posicon.TabIndex = 9
+        Me.posicon.TabStop = False
+        '
+        'productionicon
+        '
+        Me.productionicon.BackgroundImage = Global.aradan_erp.My.Resources.Resources.production_planning
+        Me.productionicon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.productionicon.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.productionicon.Location = New System.Drawing.Point(88, 193)
+        Me.productionicon.Name = "productionicon"
+        Me.productionicon.Size = New System.Drawing.Size(51, 50)
+        Me.productionicon.TabIndex = 10
+        Me.productionicon.TabStop = False
+        '
+        'purchaseicon
+        '
+        Me.purchaseicon.BackgroundImage = Global.aradan_erp.My.Resources.Resources.purchase_icon
+        Me.purchaseicon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.purchaseicon.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.purchaseicon.Location = New System.Drawing.Point(179, 98)
+        Me.purchaseicon.Name = "purchaseicon"
+        Me.purchaseicon.Size = New System.Drawing.Size(51, 50)
+        Me.purchaseicon.TabIndex = 11
+        Me.purchaseicon.TabStop = False
+        '
+        'Welcome
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.aradan_erp.My.Resources.Resources.aradan_desktop_erp_no_icons
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(684, 661)
+        Me.Controls.Add(Me.purchaseicon)
+        Me.Controls.Add(Me.productionicon)
+        Me.Controls.Add(Me.posicon)
+        Me.Controls.Add(Me.crmicon)
+        Me.Controls.Add(Me.salesicon)
+        Me.Controls.Add(Me.ecommerceicon)
+        Me.Controls.Add(Me.biicon)
+        Me.Controls.Add(Me.fleeticon)
+        Me.Controls.Add(Me.hricon)
+        Me.Controls.Add(Me.payrollicon)
+        Me.Controls.Add(Me.realestateicon)
+        Me.Controls.Add(Me.inventoryicon)
+        Me.Name = "Welcome"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Welcome"
+        CType(Me.inventoryicon, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.realestateicon, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.payrollicon, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.hricon, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.fleeticon, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.biicon, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ecommerceicon, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.salesicon, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.crmicon, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.posicon, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.productionicon, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.purchaseicon, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents inventoryicon As System.Windows.Forms.PictureBox
+    Friend WithEvents realestateicon As System.Windows.Forms.PictureBox
+    Friend WithEvents payrollicon As System.Windows.Forms.PictureBox
+    Friend WithEvents hricon As System.Windows.Forms.PictureBox
+    Friend WithEvents fleeticon As System.Windows.Forms.PictureBox
+    Friend WithEvents biicon As System.Windows.Forms.PictureBox
+    Friend WithEvents ecommerceicon As System.Windows.Forms.PictureBox
+    Friend WithEvents salesicon As System.Windows.Forms.PictureBox
+    Friend WithEvents crmicon As System.Windows.Forms.PictureBox
+    Friend WithEvents posicon As System.Windows.Forms.PictureBox
+    Friend WithEvents productionicon As System.Windows.Forms.PictureBox
+    Friend WithEvents purchaseicon As System.Windows.Forms.PictureBox
+
+End Class
---
+++ aradan_erp/Welcome.vb
@@ -0,0 +1,12 @@
+﻿Public Class Welcome
+
+    Private Sub realestateicon_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles realestateicon.Click
+
+    End Sub
+
+    Private Sub posicon_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles posicon.Click
+        Dim posLayoutForm As New aradan_erp.PosLayoutForm()
+        posLayoutForm.Show()
+        Me.Hide()
+    End Sub
+End Class
